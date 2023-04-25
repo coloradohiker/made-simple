11b6ad07f1089070b033e737055319be993b1492
coloradohiker <54600281+coloradohiker@users.noreply.github.com>
2023-04-25 13:45:09 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Made + Simple`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
